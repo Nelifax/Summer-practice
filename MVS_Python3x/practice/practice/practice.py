2a1a4d2fed2dd5ca1ddd4392726e6afd7c518b7d
Nelifax <108386670+Nelifax@users.noreply.github.com>
2022-06-29 07:56:26 +0200

    Add files via upload

---
+++ MVS_Python3x/practice/practice/practice.py
@@ -0,0 +1,9 @@
+﻿stroke=input()
+max=stroke[0]
+for i in range(0,len(stroke)-1):
+    for j in range(len(stroke),i,-1):
+        comp=stroke[i:j]
+        if comp == comp[::-1]:
+            if len(max)<len(comp):
+                max=comp
+print(max)
